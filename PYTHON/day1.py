5601be784f4f4094347be68ae7f5323fa61b41b3
Sudip Bharati <sudipjungbharatii@gmail.com>
2026-02-18 20:13:05 +0545

    Add files via upload

---
+++ PYTHON/day1.py
@@ -0,0 +1,23 @@
+
+#python character set
+#letters - a-z, A-Z
+#digits - 0-9
+#underscore - _
+#cannot start with a digit
+#cannot use reserved keywords
+#examples of valid variable names
+
+print("my name is sudip" , "my age is 20")
+print(33 + 44)
+
+
+#---variable----
+name = "sudip"
+age = 20
+print("my name is", name, "and my age is", age)
+
+#------data type----------
+name = "sudip"
+print(type(name))
+# integers, floats, strings, booleans, none
+
