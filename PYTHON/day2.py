138b49ec063c9d201ca8364b64f034e944353826
Sudip Bharati <sudipjungbharatii@gmail.com>
2026-02-19 21:08:38 +0545

    Add files via upload

---
+++ PYTHON/day2.py
@@ -0,0 +1,14 @@
+#installationn of modules.day 3 od code with harry
+import pandas as pd
+print("My name is sudip Bharati")
+
+#day 4 od code with harry
+#print is a function.
+print("hello")
+print("hi")
+print("Bye")
+print(13*13)
+
+#task of day 4
+#print a song of your choice
+print("Sayau thunga ful ka hami euta mala npali")
