a07018b0679ea4722844b82fd7dd4f4689ea738f
Sudip Bharati <sudipjungbharatii@gmail.com>
2026-02-21 21:28:41 +0545

    Add files via upload

---
+++ PYTHON/day4.py
@@ -0,0 +1,21 @@
+print("===== MY PORTFOLIO =====")
+
+print("\nName: Sudip Bharati")
+print("Location: Nepal")
+
+print("\n--- About Me ---")
+print("I am a beginner Python learner.")
+print("I am learning web development and GitHub.")
+
+print("\n--- Skills ---")
+print("Python")
+print("HTML")
+print("CSS")
+
+print("\n--- Projects ---")
+print("1. Simple Calculator")
+print("2. Currency Converter Website")
+
+print("\n--- Contact ---")
+print("Email: sudipjungbharatii@gmail.com")
+print("GitHub: github.com/sudipbharati-20")
