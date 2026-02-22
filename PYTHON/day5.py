8b2179639c5bbd7bc1f36b872a5d46199757faff
Sudip Bharati <sudipjungbharatii@gmail.com>
2026-02-22 22:27:08 +0545

    Add files via upload

---
+++ PYTHON/day5.py
@@ -0,0 +1,16 @@
+#day 6 in code with harry
+# a = 1
+# print(a)
+# b = "Sudip Bharati"
+# print(b)
+
+# print("The tyype of a is ", type(a))
+# print("The type of b is ", type(b))
+
+#---------list--------------
+# list1 = [4, 4, 4, 4]
+# print(list1)
+
+#-------dictionary--------
+# dict1 = {"name":"sakshi", "age":20, "canvote":True}
+sa()
