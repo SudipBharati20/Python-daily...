14ca408a0f6ea8dd237fb957efca63e4db3f9647
Sudip Bharati <sudipjungbharatii@gmail.com>
2026-02-25 15:27:26 +0545

    Add files via upload

---
+++ PYTHON/day8.py
@@ -0,0 +1,53 @@
+# # # #write a program thata asks a user two enter two number A and B and then code to swap your values of A and B and print them out
+# # # FirstNumber = int(input("Enter the first number (A): "))
+# # # SecondNumber = int(input("Enter the second number (B): "))
+# # # print("Number before swapping: A =", FirstNumber, "B =", SecondNumber)
+# # # # Swapping the values
+# # # temp = FirstNumber
+# # # FirstNumber = SecondNumber
+# # # SecondNumber = temp
+# # # print("Number after swapping: A =", FirstNumber, "B =", SecondNumber)
+
+# # #write a program that asks the user to enter the name anf the prices of three items theya re purchasing
+# # #the program should
+# # #calculate the total cost of the three item
+# # #calculate the tax amount(13%)
+# # #display a nicely formatted receipt showing
+# # #each item and its price
+# # #the subtotal, tax, and final total
+# # item1_name = str(input("Enter the name of the first item: "))
+# # item1_price = float(input("Enter the price of the first item: "))
+# # item2_name = str(input("Enter the name of the second item: "))
+# # item2_price = float(input("Enter the price of the second item: "))
+# # item3_name = str(input("Enter the name of the third item: "))
+# # item3_price = float(input("Enter the price of the third item: "))
+# # subtotal = item1_price + item2_price + item3_price
+# # tax = subtotal * 0.13
+# # print("tax amount is: ", tax)
+# # total = subtotal + tax
+# # print("Total cost after tax: ", total)
+# # print("Receipt: here is your bill...", total )
+
+# #students marks calculator
+
+# subject1 = int(input("Enter your marks: "))
+# subject2 = int(input("Enter your marks: "))
+# subject3 = int(input("Enter your marks: "))
+
+# Total = subject1 + subject2 + subject3
+# average = Total/3
+# print("your subject1 mark is:", subject1)
+# print("your subject2 mark is:", subject2)
+# print("your subject3 mark is:", subject3)
+# print("Your total marks is: ", Total)
+# print("Your average marks is: ", average)
+
+#simple salary calculator
+#write a program that asks the user to enter
+#-employee name
+#-basic salary
+#The program should
+#-calculate allowances(10% of basic salary)
+    #calculate tax(5% of basic salary)
+#calculate net salary
+#display a salary slip with all details
