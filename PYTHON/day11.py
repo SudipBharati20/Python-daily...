913a0e90c4e11f26130ebf705b1a75ac25100a6c
Sudip Bharati <sudipjungbharatii@gmail.com>
2026-02-28 17:22:30 +0545

    Add files via upload

---
+++ PYTHON/day11.py
@@ -0,0 +1,24 @@
+import random
+
+print("Welcome to Rock Paper Scissors!")
+
+choices = ["rock", "paper", "scissors"]
+
+user_choice = input("Enter rock, paper, or scissors: ").lower()
+computer_choice = random.choice(choices)
+
+print("Computer chose:", computer_choice)
+
+if user_choice in choices:
+    if user_choice == computer_choice:
+        print("It's a tie!")
+    elif user_choice == "rock" and computer_choice == "scissors":
+        print("You win!")
+    elif user_choice == "paper" and computer_choice == "rock":
+        print("You win!")
+    elif user_choice == "scissors" and computer_choice == "paper":
+        print("You win!")
+    else:
+        print("Computer wins!")
+else:
+    print("Invalid input. Please run again and choose rock, paper, or scissors.")
