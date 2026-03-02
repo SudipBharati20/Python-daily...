720ab7465aa35645f099fbe345a7cbfd5730c72e
Sudip Bharati <sudipjungbharatii@gmail.com>
2026-03-02 20:32:21 +0545

    Add files via upload

---
+++ PYTHON/day13.py
@@ -0,0 +1,41 @@
+# #code with harry ep-07
+# #operators
+# a=10
+# b=5
+# print("Addition:", a + b)
+# print("Subtraction:", a - b)
+# print("Multiplication:", a * b)
+# print("Division:", a / b)
+# print("Floor Division:", a // b)
+# print("Modulus:", a % b)
+# print("Exponentiation:", a ** b)
+
+# #create a calculator capable of performing addition, subtraction, multiplication, and division based on user input.
+# print("Simple Calculator")
+# num1 = float(input("Enter first number: "))
+# num2 = float(input("Enter second number: "))
+# operation = input("Enter operation (+, -, *, /): ")
+
+# if operation == "+":
+#     result = num1 + num2
+# elif operation == "-":
+#     result = num1 - num2
+# elif operation == "*":
+#     result = num1 * num2
+# elif operation == "/":
+#     if num2 != 0:
+#         result = num1 / num2
+#     else:
+#         print("Error: Division by zero is not allowed.")
+#         exit()
+# else:
+#     print("Invalid operation.")
+#     exit()
+
+# print(f"Result: {result}")
+
+#----------------day8-----------------------#
+a= 56
+b= 3
+print("The value of ", a,"+", b, "is:", a + b)
+
