f6434fb2b2d4ac8e255db6ec191b79528eeb4d3b
Sudip Bharati <sudipjungbharatii@gmail.com>
2026-03-04 05:05:17 +0545

    Add files via upload

---
+++ PYTHON/day15_1.py
@@ -0,0 +1,12 @@
+#-----string slicing-----------#
+
+#counting starts from 0
+
+name = "Sudip Bharati"
+print(name[0:2])
+print(name[2:])
+print(name[:4])
+print(name[::2])
+print(name[2::])
+print(name[::-1])
+
