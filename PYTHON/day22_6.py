712c6aeefd35e4e5c9d8a752b852c52cbccb8ce1
Sudip Bharati <sudipjungbharatii@gmail.com>
2026-03-11 15:22:04 +0545

    Add files via upload

--- PYTHON/day22_6.py
+++ PYTHON/day22_6.py
@@ -1,10 +1,17 @@
-n = int(input("Enter how many terms: "))
+#write a python program that:
+#in the given list:
+#[1,2,6,4,5,7,1,4,5]
+#counts how many times each number appears
+#displays only the number that are repeated  and how many times they appear.
+input_list=[1,2,6,4,5,7,1,4,5]
+count_dict={}
+for number in input_list:
+    if number in count_dict:
+        count_dict[number]+=1
+    else:
+        count_dict[number]=1
 
-a = 0
-b = 1
-
-for i in range(n):
-    print(a, end=" ")
-    c = a + b
-    a = b
-    b = c
+for number, count in count_dict.items():
+    if count > 1:
+        print(f"{number}: {count}")
+        
