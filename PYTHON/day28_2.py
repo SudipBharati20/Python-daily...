7a9aab4715a7ea985aa47f945a43b086d6e240a0
Sudip Bharati <sudipjungbharatii@gmail.com>
2026-03-17 19:50:17 +0545

    Add files via upload

---
+++ PYTHON/day28_2.py
@@ -0,0 +1,14 @@
+print("Welcone to my store")
+print("1.View Data\n2.Add Data\n3.Delete Data\n4.Exit")
+
+while True:
+ number= int(input("Enter the number fo the things you want to print"))
+ if(number==1):
+    print("View Data")
+ elif(number==2):
+    print("Add Data")
+ elif(number==3):
+    print("Delete Data")
+ elif(number==4):
+    print("Exit")
+    break
