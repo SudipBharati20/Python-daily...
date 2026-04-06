dc7f0427fb966abc188c5313138287130ed820c3
Sudip Bharati <sudipjungbharatii@gmail.com>
2026-04-06 20:36:54 +0545

    Add files via upload

---
+++ PYTHON/day42_2.py
@@ -0,0 +1,13 @@
+from sklearn.linear_model import LinearRegression
+import numpy as np
+
+# Sample data
+X = np.array([[1], [2], [3], [4], [5]])
+y = np.array([2, 4, 6, 8, 10])
+
+model = LinearRegression()
+model.fit(X, y)
+
+# Predict
+prediction = model.predict([[6]])
+print("Prediction:", prediction)
