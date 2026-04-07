3ad24034aeff28cdabfc853ad1fc9f3f4939bde0
Sudip Bharati <sudipjungbharatii@gmail.com>
2026-04-07 11:11:02 +0545

    Add files via upload

---
+++ PYTHON/day43_3.py
@@ -0,0 +1,10 @@
+#write a program that opens file name as "data.txt" and read the content of the file and print it to the console.
+#open the file in read mode
+file = open("data.txt", "r")
+print(file.read())
+#close the file
+file.close()
+
+
+
+
